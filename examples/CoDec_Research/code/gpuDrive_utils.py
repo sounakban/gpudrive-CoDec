4aa97ae54e5b3d61dc1f32c2f1f6eb5f73d585b7
Sounak Banerjee <bsounak93@gmail.com>
2025-05-13 20:44:17 -0400

    Server optimization

--- examples/CoDec_Research/code/gpuDrive_utils.py
+++ examples/CoDec_Research/code/gpuDrive_utils.py
@@ -36,7 +36,7 @@ def get_gpuDrive_vars(training_config,
                       env: GPUDriveConstrualEnv = None,
                       env_multi_agent: GPUDriveConstrualEnv = None,
                       ):
-    # |Create environment config
+    # |Create environment config (for default device)
     env_config = dataclasses.replace(
         EnvConfig(),
         ego_state=training_config.ego_state,
@@ -47,7 +47,31 @@ def get_gpuDrive_vars(training_config,
         dynamics_model=training_config.dynamics_model,
         collision_behavior=training_config.collision_behavior,
         dist_to_goal_threshold=training_config.dist_to_goal_threshold,
-        # polyline_reduction_threshold=0.2 if device == "cpu" else training_config.polyline_reduction_threshold,
+        polyline_reduction_threshold=0.2 if device == "cpu" else training_config.polyline_reduction_threshold,
+        polyline_reduction_threshold=0.2,
+        remove_non_vehicles=training_config.remove_non_vehicles,
+        lidar_obs=training_config.lidar_obs,
+        disable_classic_obs=training_config.lidar_obs,
+        obs_radius=training_config.obs_radius,
+        steer_actions = torch.round(
+            torch.linspace(-torch.pi, torch.pi, training_config.action_space_steer_disc), decimals=3  
+        ),
+        accel_actions = torch.round(
+            torch.linspace(-4.0, 4.0, training_config.action_space_accel_disc), decimals=3
+        ),
+    )
+
+    # |Create environment config (for the CPU version of the environment)
+    env_config_cpu = dataclasses.replace(
+        EnvConfig(),
+        ego_state=training_config.ego_state,
+        road_map_obs=training_config.road_map_obs,
+        partner_obs=training_config.partner_obs,
+        reward_type=training_config.reward_type,
+        norm_obs=training_config.norm_obs,
+        dynamics_model=training_config.dynamics_model,
+        collision_behavior=training_config.collision_behavior,
+        dist_to_goal_threshold=training_config.dist_to_goal_threshold,
         polyline_reduction_threshold=0.2,
         remove_non_vehicles=training_config.remove_non_vehicles,
         lidar_obs=training_config.lidar_obs,
@@ -93,7 +117,7 @@ def get_gpuDrive_vars(training_config,
         #   (Multiple initializations may lead to segmentation fault)
         # |Create multi-agent environment to get information about moving vehicles
         env_multi_agent = GPUDriveConstrualEnv(
-                            config=env_config,
+                            config=env_config_cpu,
                             data_loader=train_loader,
                             max_cont_agents=max_agents,
                             device="cpu",
