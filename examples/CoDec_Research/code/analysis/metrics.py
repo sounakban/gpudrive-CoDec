43e1260f2eacf0dad88c0c1bff9b5478dc9f6948
Sounak Banerjee <bsounak93@gmail.com>
2025-06-30 11:59:51 -0400

    Server commit to run operation with 90 trajectory samples + Sample analysis code

--- examples/CoDec_Research/code/analysis/metrics.py
+++ examples/CoDec_Research/code/analysis/metrics.py
@@ -12,18 +12,3 @@ def average_displacement_error(pred, gt):
         float: Average displacement error.
     """
     return torch.mean(torch.sqrt(torch.sum((pred - gt) ** 2, dim=1))).item()
-
-
-
-def log_likelihood(pred, gt): ####FIXME
-    """
-    Calculate the log likelihood of the predicted points given the ground truth points.
-
-    Args:
-        pred (torch.Tensor): Predicted points of shape (N, M).
-        gt (torch.Tensor): Ground truth points of shape (N, M).
-
-    Returns:
-        float: Log likelihood.
-    """
-    return -0.5 * torch.sum((pred - gt) ** 2).item()
